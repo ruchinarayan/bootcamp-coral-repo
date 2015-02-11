65b27d7ed619426f801115897e1a20ac814fd5b4
Ruchi Ruchi <ruchinarayan@gmail.com>
2015-02-11 20:38:33 +0000

    hw4 task4 Commit- rruchi

---
+++ app/models/review.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :integer          not null, primary key
+#  stars      :integer
+#  title      :string
+#  commentary :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  Task done by rruchi
+#
+
+class Review < ActiveRecord::Base
+	 validates :stars, presence: true, inclusion:{in: 0..5} 
+	 validates :title, presence: true, length: {maximum: 100}
+	 validates :commentary, presence: true, length: {maximum: 10000}
+end
--- test/controllers/my_static_pages_controller_test.rb
+++ test/controllers/my_static_pages_controller_test.rb
@@ -1,15 +1,14 @@
 require 'test_helper'
 
 class MyStaticPagesControllerTest < ActionController::TestCase
-<<<<<<< HEAD
+
   test "should get mfshamimTask2a" do
     get :mfshamimTask2a
     assert_response :success
   end
 
-=======
   # test "the truth" do
   #   assert true
   # end
->>>>>>> hw3_task5
+
 end
